ffcfe1811f30ec9fad8c5ac7e6cb527d121ce5df
Jane Vasylieva <janekanso86@gmail.com>
2025-06-19 00:13:30 +0300

    add3

--- src/components/ImageModal/ImageModal.tsx
+++ src/components/ImageModal/ImageModal.tsx
@@ -1,15 +1,19 @@
-import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
-export default function ImageModal({ isOpen, onClose, image }) {
+import Modal from 'react-modal';
+import { ImageData } from '../../types';
+
+interface Props {
+  isOpen: boolean;
+  onClose: () => void;
+  image: ImageData;
+}
+
+export default function ImageModal({ isOpen, onClose, image }: Props) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} contentLabel="Image Modal">
-      <div onClick={onClose}>
-        <img src={image.urls.regular} alt={image.alt_description} />
-        <p>Author: {image.user.name}</p>
-        <p>Likes: {image.likes}</p>
-        <p>Description: {image.alt_description}</p>
-      </div>
+    <Modal isOpen={isOpen} onRequestClose={onClose}>
+      <img src={image.largeImageURL} alt={image.altText} />
+      <button onClick={onClose}>Close</button>
     </Modal>
   );
 }
